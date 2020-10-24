e5954e024e226279931394483d043611865c8218
Love Otudor <loveotudor@gmail.com>
2020-10-25 00:11:58 +0100

    Passing data using Safe Args

--- app/src/main/java/com/example/navigationsampleapp/HomeFragment.java
+++ app/src/main/java/com/example/navigationsampleapp/HomeFragment.java
@@ -1,25 +1,24 @@
 package com.example.navigationsampleapp;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
+import java.text.DecimalFormat;
 
 public class HomeFragment extends Fragment {
 
-    public static HomeFragment newInstance() {
-        return new HomeFragment();
-    }
+    DecimalFormat df = new DecimalFormat("###.###");
+
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -37,13 +36,30 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button button = view.findViewById(R.id.next_fragment_button);
+        final EditText lengthInCmEt = view.findViewById(R.id.length_in_cm);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(HomeFragmentDirections.launchSecondFragment());
+                Navigation.findNavController(view).navigate(HomeFragmentDirections.launchSecondFragment().setLengthInFeet(convertToFeet(lengthInCmEt.getText().toString())));
             }
         });
 
+    }
+
+    public String convertToFeet(String lengthInCm){
+        double lengthCm;
+        String result;
+
+        try {
+            lengthCm = Double.parseDouble(lengthInCm);
+            double lengthInFt = lengthCm/30.48;
+            result = df.format(lengthInFt) +" ft";
+
+        } catch (NumberFormatException e) {
+            result = null;
+        }
+
+        return result;
     }
 
 }
